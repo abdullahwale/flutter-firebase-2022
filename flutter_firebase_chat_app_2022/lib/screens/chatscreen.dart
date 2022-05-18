84d33e22d1ada1ded7815ca3a14d9315295f821d
abdullahwale <abdullahwale@gmail.com>
2022-05-18 15:21:44 +0500

    Add Data and Stream Builder

--- flutter_firebase_chat_app_2022/lib/screens/chatscreen.dart
+++ flutter_firebase_chat_app_2022/lib/screens/chatscreen.dart
@@ -7,28 +7,44 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (cont, index) {
-            return Container(
-              padding: EdgeInsets.all(15),
-              child: Text("Item's Created"),
-            );
+      body: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('chats/IoR4uT3o7Bb2s9SmErJt/message')
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            final documents = snapshot.data!.docs;
+            return ListView.builder(
+                itemCount: documents.length,
+                itemBuilder: (cont, index) {
+                  return Container(
+                    padding: EdgeInsets.all(15),
+                    child: Text(documents[index]['text']),
+                  );
+                });
           }),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.accessibility_rounded),
         onPressed: () {
           FirebaseFirestore.instance
               .collection('chats/IoR4uT3o7Bb2s9SmErJt/message')
-              .snapshots()
-              .listen((event) {
-            event.docs.forEach((element) {
-              print(element['text']);
-            });
-            //print(event.docs[0]['text']);
-          });
+              .add({'text': "COMSATS"});
         },
       ),
     );
   }
 }
+
+// FirebaseFirestore.instance
+//     .collection('chats/IoR4uT3o7Bb2s9SmErJt/message')
+//     .snapshots()
+//     .listen((event) {
+//   event.docs.forEach((element) {
+//     print(element['text']);
+//   });
+//   //print(event.docs[0]['text']);
+// });
